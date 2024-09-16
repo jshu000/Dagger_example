17e7e2cd5dd4ab0b7f72f0a86c54a2daa6ba153c
Jshu <56064412+jshu000@users.noreply.github.com>
2024-09-16 18:59:02 +0530

    Initial commit

---
+++ app/src/main/java/com/jashwant/dagger_example/EmailService.kt
@@ -0,0 +1,9 @@
+package com.jashwant.dagger_example
+
+import android.util.Log
+
+class EmailService {
+    fun send(to:String, body:String?){
+        Log.d("jashwant", "Email sent-")
+    }
+}
---
+++ app/src/main/java/com/jashwant/dagger_example/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.jashwant.dagger_example
+
+import android.util.Log
+
+class UserRepository {
+    fun saveUser(email:String,password:String){
+        Log.d("jashwant", "saveUser: email-")
+    }
+}
