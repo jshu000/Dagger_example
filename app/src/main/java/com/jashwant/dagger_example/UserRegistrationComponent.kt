5f3d64976b64067585b18235a9956f373f7ce063
Jshu <56064412+jshu000@users.noreply.github.com>
2024-11-28 00:50:17 +0530

    Error removed
    Error removed

--- app/src/main/java/com/jashwant/dagger_example/UserRegistrationComponent.kt
+++ app/src/main/java/com/jashwant/dagger_example/UserRegistrationComponent.kt
@@ -5,7 +5,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(dependencies = , modules =[UserRepositoryModule::class, NotificationServiceModule::class,AnalyticsModule::class])
+@Component( modules =[UserRepositoryModule::class, NotificationServiceModule::class,AnalyticsModule::class])
 interface UserRegistrationComponent {
 
 /*    fun getUserRegistrationService() : UserRegistrationService
